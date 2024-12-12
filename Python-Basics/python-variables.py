73fafa44de92a53f093dd81b1753024728426099
ArifeLjutviovska <56047137+ArifeLjutviovska@users.noreply.github.com>
2024-12-12 21:00:21 +0100

    Added python-variables file
    In this PR, I tested the variables in python

---
+++ Python-Basics/python-variables.py
@@ -0,0 +1,9 @@
+age = 27 #number
+print('My age is', age)
+first_name = "Arife" #string
+print ("My first name is", first_name)
+height=160.5 #decimal
+print("My height is",160,"cm")
+can_learn_python = True ##boolean, with upper case True/False, not with lower case
+print("So, I will learn python?", can_learn_python)
+ 
